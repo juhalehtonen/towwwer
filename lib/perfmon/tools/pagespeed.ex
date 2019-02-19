b3a353054317aad6dc02c5ad8e39dda59a6954b4
Juha Lehtonen <hello@juha.tl>
2019-02-19 13:06:49 +0200

    Add helpers to display perf scores, URL changes to fetch them

--- lib/perfmon/tools/pagespeed.ex
+++ lib/perfmon/tools/pagespeed.ex
@@ -1,8 +1,9 @@
 defmodule PerfMon.Tools.PageSpeed do
   @api_base_url "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url="
+  @api_categories "&category=performance&category=pwa&category=best-practices&category=accessibility&category=seo"
 
   def query_pagespeed_api(url) when is_binary(url) do
-    case HTTPoison.get(@api_base_url <> url) do
+    case HTTPoison.get(@api_base_url <> url <> @api_categories) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         {:ok, body}
       {:error, %HTTPoison.Error{reason: reason}} ->
--- lib/perfmon_web/views/site_view.ex
+++ lib/perfmon_web/views/site_view.ex
@@ -1,3 +1,10 @@
 defmodule PerfMonWeb.SiteView do
   use PerfMonWeb, :view
+
+  def score(report, type) when type in ["performance", "seo", "accessibility", "best-practices", "pwa"] do
+    case report.data["lighthouseResult"]["categories"][type]["score"] do
+      nil -> 0
+      _ -> report.data["lighthouseResult"]["categories"][type]["score"] * 100 |> round()
+    end
+  end
 end
