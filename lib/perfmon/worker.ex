795cbf0e69dd70cae90b9930128ca8d4a982ca8e
Juha Lehtonen <hello@juha.tl>
2019-02-19 10:52:09 +0200

    Write down TODO

---
+++ lib/perfmon/worker.ex
@@ -0,0 +1,7 @@
+defmodule PerfMon.Worker do
+
+  # TODO: Periodically query every Site and send API request for their Monitor(s) path(s)
+  # Then create a new Report associated with given Monitor, saving the body of the response
+  # as the data in the Report.
+
+end
