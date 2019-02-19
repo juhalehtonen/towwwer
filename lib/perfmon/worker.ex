d12251e5bcdf712aeb20f3403af39179481e83fa
Juha Lehtonen <hello@juha.tl>
2019-02-19 11:28:09 +0200

    Prototype saving reports

--- lib/perfmon/worker.ex
+++ lib/perfmon/worker.ex
@@ -1,7 +1,20 @@
 defmodule PerfMon.Worker do
+  alias PerfMon.Tools.PageSpeed
+  alias PerfMon.Websites.Report
+  alias PerfMon.Websites
 
   # TODO: Periodically query every Site and send API request for their Monitor(s) path(s)
   # Then create a new Report associated with given Monitor, saving the body of the response
   # as the data in the Report.
+
+  def lol(url) do
+    case PageSpeed.query_pagespeed_api(url) do
+      {:ok, body} ->
+        data = Jason.decode!(body)
+        Websites.create_report(%{data: data})
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
 
 end
