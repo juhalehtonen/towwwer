c32eadd23b98033001fede6ecde81c9ac58ae6c6
Juha Lehtonen <hello@juha.tl>
2019-02-23 15:32:19 +0200

    Add preliminary Seed sample file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,17 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+
+alias PerfMon.Websites
+alias PerfMon.Websites.Site
+alias PerfMon.Websites.Monitor
+
+sites= [
+  %{base_url: "https://juha.tl", token: "asd", monitors: [%{}]},
+  %{base_url: "https://www.evermade.fi", token: "asd", monitors: [%{}]},
+]
+
+for site <- sites do
+    Websites.create_site(site)
+end
