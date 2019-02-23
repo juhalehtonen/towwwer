391500d5ad5f5c4fc2016fd566fdbb5aec5bbe01
Juha Lehtonen <hello@juha.tl>
2019-02-23 22:46:18 +0200

    Update all seed sites to their correct URLs

--- lib/perfmon/tools/apiclient.ex
+++ lib/perfmon/tools/apiclient.ex
@@ -3,10 +3,10 @@ defmodule PerfMon.Tools.ApiClient do
 
   @fuse_name __MODULE__
   @fuse_options [
-    # Tolerate 10 failures for every 1 second time window.
-    fuse_strategy: {:standard, 10, 1000},
-    # Reset the fuse 10 seconds after it is blown.
-    fuse_refresh: 10000,
+    # Tolerate 30 failures for every 1 second time window.
+    fuse_strategy: {:standard, 30, 1000},
+    # Reset the fuse 5 seconds after it is blown.
+    fuse_refresh: 5000,
     # Limit to 60 calls per 100 seconds
     rate_limit: {60, 100_000}
   ]
@@ -38,10 +38,11 @@ defmodule PerfMon.Tools.ApiClient do
     |> PageSpeed.query_pagespeed_api()
     |> case do
          {:error, status_code} when status_code in @retry_errors ->
+           IO.inspect url
            IO.inspect {:retry, status_code}
          # If not a retriable error, just return the result.
          pagespeed_result ->
-           IO.inspect pagespeed_result
+           pagespeed_result
        end
   end
 end
