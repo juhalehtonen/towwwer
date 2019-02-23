299615dde2eddfad00db2859cf28d58dd47ac732
Juha Lehtonen <hello@juha.tl>
2019-02-23 10:58:19 +0200

    Add ability to delete individual monitors

--- lib/perfmon/websites/monitor.ex
+++ lib/perfmon/websites/monitor.ex
@@ -7,6 +7,7 @@ defmodule PerfMon.Websites.Monitor do
 
   schema "monitors" do
     field :path, :string, default: "/"
+    field :delete, :boolean, virtual: true # Not persisted to database
     belongs_to :site, Site
     has_many :reports, Report
 
@@ -16,7 +17,16 @@ defmodule PerfMon.Websites.Monitor do
   @doc false
   def changeset(monitor, attrs) do
     monitor
-    |> cast(attrs, [:path])
+    |> cast(attrs, [:path, :delete])
+    |> set_delete_action()
     |> validate_required([:path])
+  end
+
+  defp set_delete_action(changeset) do
+    if get_change(changeset, :delete) do
+      %{changeset | action: :delete}
+    else
+      changeset
+    end
   end
 end
