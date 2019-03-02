043a0cdcdbbffa065b4c380ec91c89e3e5e9bad3
Juha Lehtonen <hello@juha.tl>
2019-03-02 20:22:55 +0200

    Cleanup calls to removed code

--- lib/perfmon_web/views/site_view.ex
+++ lib/perfmon_web/views/site_view.ex
@@ -44,24 +44,6 @@ defmodule PerfMonWeb.SiteView do
     end
   end
 
-  # Timestamp of when last automated reports were generated
-  def last_automated_reports do
-    PerfMon.Worker.get_state()
-    |> NaiveDateTime.truncate(:second)
-  end
-
-  # Timestamp for when next automated reports will be generated
-  def next_automated_reports do
-    timestamp_last = last_automated_reports()
-    timestamp_now = NaiveDateTime.utc_now()
-    periodic_wait = PerfMon.Worker.periodic_wait()
-    diff_in_ms = NaiveDateTime.diff(timestamp_now, timestamp_last, :millisecond)
-    wait_minus_diff = periodic_wait - diff_in_ms
-
-    NaiveDateTime.add(NaiveDateTime.utc_now(), wait_minus_diff, :millisecond)
-    |> NaiveDateTime.truncate(:second)
-  end
-
   def reports_of_monitor(monitor) do
     PerfMon.Websites.list_reports_of_monitor(monitor)
   end
