53bb7afba012435de3a89362f9957cb8a2d24423
Juha Lehtonen <hello@juha.tl>
2019-03-13 10:19:17 +0200

    Do not check for db updates when running WPScan.
    This means that we need to run the database updates outside of the regular jobs.

--- lib/towwwer/tools/wpscan.ex
+++ lib/towwwer/tools/wpscan.ex
@@ -8,7 +8,7 @@ defmodule Towwwer.Tools.WPScan do
   Run WPScan against the given `url` with default settings.
   """
   def run(url) when is_binary(url) do
-    {cmd, _} = System.cmd("wpscan", ["--no-banner", "--force", "--format", "json", "--url", url], parallelism: true)
+    {cmd, _} = System.cmd("wpscan", ["--no-banner", "--force", "---no-update", "--format", "json", "--url", url], parallelism: true)
     {:ok, cmd}
   end
   def run(_url), do: {:error, "URL not a binary"}
