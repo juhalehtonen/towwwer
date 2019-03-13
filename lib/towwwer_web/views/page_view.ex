bfc9466c767bab8fa2462d9132771275c3c000fb
Juha Lehtonen <hello@juha.tl>
2019-03-13 10:59:44 +0200

    Add checks for core and plugin vulnerabilities

--- lib/towwwer_web/views/page_view.ex
+++ lib/towwwer_web/views/page_view.ex
@@ -5,4 +5,26 @@ defmodule TowwwerWeb.PageView do
     report = Enum.at(monitor.reports, 0)
     report.wpscan_data["interesting_findings"]
   end
+
+  def core_vulns(monitor) do
+    report = Enum.at(monitor.reports, 0)
+
+    if report.wpscan_data["version"] do
+      core_info = report.wpscan_data["version"]
+      core_info["vulnerabilities"]
+    else
+      []
+    end
+  end
+
+  def vulns(monitor) do
+    report = Enum.at(monitor.reports, 0)
+
+    vulns = report.wpscan_data["plugins"]
+    |> Enum.filter(fn({plugin_name, plugin_data}) ->
+      current_plugin_version = plugin_data["version"]
+      current_vulnerabilities = current_plugin_version["vulnerabilities"]
+      current_vulnerabilities != [] && current_vulnerabilities != nil
+    end)
+  end
 end
