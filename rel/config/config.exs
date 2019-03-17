bdb93544fe95bfdabf86ed5bdf86213d14f81167
Juha Lehtonen <hello@juha.tl>
2019-03-17 08:09:59 +0200

    Better runtime config template. WIP node for build

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -1,10 +1,23 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
 
-# ONLY include PRODUCTION RELEASE configuration here
+# ONLY include PRODUCTION RELEASE configuration here, and
+# REMEMBER to change everything to their correct values.
 
 # General application configuration
 use Mix.Config
+
+# Endpoint configuration
+config :towwwer, TowwwerWeb.Endpoint,
+  secret_key_base: "abcdefg",
+  http: [:inet6, port: 4000],
+  url: [host: "example.com", port: 4000],
+  server: true,
+  root: ".",
+  cache_static_manifest: "priv/static/cache_manifest.json"
+
+# Do not print debug messages in production
+config :logger, level: :info
 
 # Configure your database
 config :towwwer, Towwwer.Repo,
